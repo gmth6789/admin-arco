60a57fdf82ab2f8276116f7b6d4e353718119ed2
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:19:43 +0700

    1 file added

---
+++ components/mission-ufa.vue
@@ -0,0 +1,92 @@
+
+
+<template>
+
+<div class="flex diygw-col-24">
+					<div class="diygw-grid col-5">
+						<div @click="navigateTo" data-type="openmodal" class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_rm.png" />
+								</div>
+								<div class="diygw-grid-title">热门</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_aq.png" />
+								</div>
+								<div class="diygw-grid-title">法律</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_gw.png" />
+								</div>
+								<div class="diygw-grid-title">营销</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_fs.png" />
+								</div>
+								<div class="diygw-grid-title">文学</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_pxb.png" />
+								</div>
+								<div class="diygw-grid-title">排行</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_dz.png" />
+								</div>
+								<div class="diygw-grid-title">教育</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_xsqg.png" />
+								</div>
+								<div class="diygw-grid-title">商场</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_ys.png" />
+								</div>
+								<div class="diygw-grid-title">安全</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_dj.png" />
+								</div>
+								<div class="diygw-grid-title">提升</div>
+							</div>
+						</div>
+						<div class="diygw-grid-item">
+							<div class="diygw-grid-inner">
+								<div class="diygw-grid-icon diygw-avatar radius">
+									<img mode="aspectFit" class="diygw-avatar-img radius" src="./static/icon1_kf.png" />
+								</div>
+								<div class="diygw-grid-title">客服</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex diygw-col-24 line-clz">
+					<div class="diygw-pzx" style="border-bottom: 1px solid #eee"></div>
+				</div>
+  </template>
