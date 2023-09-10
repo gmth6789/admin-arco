2cc47f131d1439f0d72e5861b8722268503182e0
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:17:21 +0700

    1 file added, 1 deleted

---
+++ components/Swipe.vue
@@ -0,0 +1,22 @@
+
+<template>
+<div class="flex diygw-col-24 swiper-clz">
+					<van-swipe class="swiper swiper-indicator_rect" indicator-color="rgba(51, 51, 51, 0.39)" indicator-active-color="#fff" indicator-dots="true" autoplay="3000" interval="3000" circular="true" style="height: 150px">
+						<van-swipe-item class="diygw-swiper-item">
+							<div class="diygw-swiper-item-wrap">
+								<img src="./static/pic1.jpg" class="diygw-swiper-image" />
+							</div>
+						</van-swipe-item>
+						<van-swipe-item class="diygw-swiper-item">
+							<div class="diygw-swiper-item-wrap">
+								<img src="./static/pic2.jpg" class="diygw-swiper-image" />
+							</div>
+						</van-swipe-item>
+						<van-swipe-item class="diygw-swiper-item">
+							<div class="diygw-swiper-item-wrap">
+								<img src="./static/pic3.jpg" class="diygw-swiper-image" />
+							</div>
+						</van-swipe-item>
+					</van-swipe>
+				</div>
+	</template>
