0ca5b10a8cbb834f0145dce6b154bade87b0b7d9
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:40:10 +0700

    1 file modified, 10 added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container container24325">
-    <Nuxt />
-  
+    
   <div class="version text-grey flex-direction-column flex diygw-col-24 justify-center align-center">
 					<div>แพลตฟอร์มสร้างรายได้ออนไลน์</div>
 					<div>https://mission-th.com</div>
 				</div>
 				<div class="clearfix"></div>
   </div>
+</div>
 </template>
 
 <style>
+@import 'assets/css/home.css';
 
 </style>
