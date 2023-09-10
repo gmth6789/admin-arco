9e65ddf50fa45a1768eb2e757ea8e06d01d873cf
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:20:33 +0700

    1 file added

---
+++ components/mission-ufa2.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div class="flex diygw-col-24 items-center flex-nowrap flex37-clz">
+					<div class="flex diygw-col-0 flex-direction-column justify-between flex-nowrap flex39-clz">
+						<div class="diygw-col-24 text28-clz">เว็บยูฟ่าแจกเครดิตฟรี</div>
+						<div class="diygw-col-24 text29-clz">คนละ 200 บาท แค่สมัครสมาชิก</div>
+					</div>
+					<div class="diygw-col-0 text30-clz">ทำภารกิจ</div>
+	  </div>
+</template>
