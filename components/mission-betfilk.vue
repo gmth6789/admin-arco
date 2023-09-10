a89b4baca5b0a196a46c9931c9f6a5812ed51afa
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:25:22 +0700

    1 file added

---
+++ components/mission-betfilk.vue
@@ -0,0 +1,41 @@
+	
+ <template> 
+
+  <div class="diygw-title flex diygw-col-24">
+		<div class="title font-normal">สิทธิ์พิเศษเฉพาะคุณ</div>
+	</div>
+
+<div class="flex flex-wrap diygw-col-24 flex-direction-column flex38-clz">
+					<div class="diygw-col-24 text40-clz">แจกเงินคนละ 10 บาท</div>
+					<div class="flex diygw-col-24 items-center flex-nowrap flex54-clz">
+						<img src="./static/icon1_yhq.png" class="image9-size diygw-image diygw-col-0 image9-clz" mode="scaleToFill" />
+						<div class="flex diygw-col-0 flex-direction-column justify-between flex-nowrap flex55-clz">
+							<div class="diygw-col-24 text57-clz">1.เข้า google</div>
+							<div class="diygw-col-24 text58-clz">
+								<p><span style="font-size: 12px">ค้นหาคำว่าดูหนังออนไลน์</span></p>
+							</div>
+						</div>
+						<div class="diygw-col-0 text59-clz">ทำภารกิจ</div>
+					</div>
+					<div class="flex diygw-col-24 items-center flex-nowrap flex23-clz">
+						<img src="./static/icon1_xsqg.png" class="image10-size diygw-image diygw-col-0 image10-clz" mode="scaleToFill" />
+						<div class="flex diygw-col-0 flex-direction-column justify-between flex-nowrap flex24-clz">
+							<div class="diygw-col-24 text12-clz">免费领取流量卡</div>
+							<div class="diygw-col-24 text13-clz">30GB专属流量随心看</div>
+						</div>
+						<div class="diygw-col-0 text14-clz">ทำภารกิจ</div>
+					</div>
+					<div class="flex diygw-col-24 items-center flex-nowrap flex42-clz">
+						<img src="./static/icon1_rm.png" class="image11-size diygw-image diygw-col-0 image11-clz" mode="scaleToFill" />
+						<div class="flex diygw-col-0 flex-direction-column justify-between flex-nowrap flex53-clz">
+							<div class="diygw-col-24 text54-clz">热门会员权前每月领</div>
+							<div class="diygw-col-24 text55-clz">视频游戏50+APP免流</div>
+						</div>
+						<div class="diygw-col-0 text56-clz">ทำภารกิจ</div>
+					</div>
+				</div>
+
+
+
+
+</template>
