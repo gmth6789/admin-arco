b71372f0d9d329866bb3c69f80f1260b35d5906b
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-10 13:18:17 +0700

    1 file added

---
+++ components/Tab-icon.vue
@@ -0,0 +1,32 @@
+
+<template>
+<div class="flex flex-wrap diygw-col-24 flex12-clz">
+					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
+						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+							<img src="./static/icon1_sz1.png" class="image2-size diygw-image diygw-col-0 image2-clz" mode="widthFix" />
+							<div class="diygw-text-line1 diygw-col-0 animate__animated animate__headShake animate__infinite text9-clz">ใหม่</div>
+						</div>
+						<div class="diygw-text-line1 diygw-col-0 text8-clz">ภารกิจ</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
+						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+							<img src="./static/icon1_dz.png" class="image5-size diygw-image diygw-col-0 image5-clz" mode="widthFix" />
+						</div>
+						<div class="diygw-text-line1 diygw-col-0 text7-clz">ประวติ</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
+						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+							<img src="./static/icon1_hb.png" class="image7-size diygw-image diygw-col-0 image7-clz" mode="widthFix" />
+							<div class="diygw-text-line1 diygw-col-0 animate__animated animate__headShake animate__infinite text4-clz">ฟรี</div>
+						</div>
+						<div class="diygw-text-line1 diygw-col-0 text10-clz">เครดิตฟรี</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-6 flex-direction-column items-center">
+						<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+							<img src="./static/icon1_kf.png" class="image8-size diygw-image diygw-col-0 image8-clz" mode="widthFix" />
+						</div>
+						<div class="diygw-text-line1 diygw-col-0 text11-clz">แชท</div>
+					</div>
+				</div>
+
+  </template>
